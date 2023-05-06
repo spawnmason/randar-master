df722fec1f18086564dfb9d60db995aef52a3e8f
Babbaj <babbaj45@gmail.com>
2023-05-06 00:18:57 -0400

    look for events from the same server

--- src/main/java/net/futureclient/randar/events/EventHeartbeat.java
+++ src/main/java/net/futureclient/randar/events/EventHeartbeat.java
@@ -5,12 +5,14 @@ import com.google.gson.JsonObject;
 
 public class EventHeartbeat {
     public final long timestamp;
-    //public final String[] servers;
+    //public final String server;
+    public final String[] servers;
 
     public EventHeartbeat(JsonObject json) {
         this.timestamp = json.get("timestamp").getAsLong();
-        /*this.servers = json.get("servers").getAsJsonArray().asList().stream()
+        //this.server = json.get("server").getAsString();
+        this.servers = json.get("servers").getAsJsonArray().asList().stream()
                 .map(JsonElement::getAsString)
-                .toArray(String[]::new);*/
+                .toArray(String[]::new);
     }
 }
