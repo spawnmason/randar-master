e460b2ab241d4d91945f5251309704e90e0ef175
Babbaj <babbaj45@gmail.com>
2023-05-07 02:41:17 -0400

    minor fixes in schema

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS events (
     CHECK(id > 0)
 );
 
-CREATE TABLE event_progress (
+CREATE TABLE IF NOT EXISTS event_progress (
     id_must_be_zero INTEGER PRIMARY KEY,
     id BIGINT, -- the id of the last event we processed, so we must query > event_progress.id to get the newest events
 
@@ -14,7 +14,7 @@ CREATE TABLE event_progress (
 );
 INSERT INTO event_progress VALUES(0, 0) ON CONFLICT DO NOTHING;
 
-CREATE TABLE sqlite_backfill_progress (
+CREATE TABLE IF NOT EXISTS sqlite_backfill_progress (
     id_must_be_zero INTEGER PRIMARY KEY,
     last_rowid_processed BIGINT,
 
@@ -22,14 +22,12 @@ CREATE TABLE sqlite_backfill_progress (
 );
 INSERT INTO sqlite_backfill_progress VALUES(0, 0) ON CONFLICT DO NOTHING;
 
-CREATE TABLE servers
-(
+CREATE TABLE IF NOT EXISTS servers (
     id   SMALLSERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS worlds -- one table for both servers and dimensions because of seed being different in overworld vs nether
-(
+CREATE TABLE IF NOT EXISTS worlds ( -- one table for both servers and dimensions because of seed being different in overworld vs nether
     server_id SMALLINT    NOT NULL,
     dimension SMALLINT    NOT NULL,
     seed      BIGINT      NOT NULL,
@@ -43,7 +41,7 @@ CREATE TABLE IF NOT EXISTS worlds -- one table for both servers and dimensions b
     -- TODO add a check that worlds.id=0 means the server is 2b2t overworld, to back up the CHECK on world_id=0 in rng_seeds_processed
 );
 
-INSERT INTO servers(name) VALUES('2b2t.org');
+INSERT INTO servers(name) VALUES('2b2t.org') ON CONFLICT DO NOTHING;
 INSERT INTO worlds(server_id, dimension, seed) VALUES (0, 0, -4172144997902289642) ON CONFLICT DO NOTHING; -- 2b2t overworld
 INSERT INTO worlds(server_id, dimension, seed) VALUES (0, 1, 1434823964849314312) ON CONFLICT DO NOTHING; -- 2b2t end/nether
 
@@ -56,7 +54,7 @@ CREATE TABLE IF NOT EXISTS rng_seeds_not_yet_processed ( -- queue of seeds we ne
     UNIQUE(server_id, dimension, received_at, rng_seed),
 
     CHECK(received_at > 0),
-    CHECK(rng_seed >= 0 AND rng_seed < (1 << 48)),
+    CHECK(rng_seed >= 0 AND rng_seed < (1::bigint << 48)),
 
     FOREIGN KEY (server_id) REFERENCES servers (id)
         ON UPDATE CASCADE ON DELETE CASCADE
@@ -83,19 +81,26 @@ CREATE TABLE IF NOT EXISTS rng_seeds (
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE players
-(
+CREATE TABLE IF NOT EXISTS players (
     id       SERIAL PRIMARY KEY,
     uuid     UUID NOT NULL UNIQUE,
     username TEXT DEFAULT NULL
 );
 
-CREATE INDEX players_by_username ON players (username);
+CREATE INDEX IF NOT EXISTS players_by_username ON players (username);
+
+CREATE TABLE IF NOT EXISTS name_history (
+  player_id INTEGER NOT NULL,
+  username  TEXT NOT NULL,
+  time      BIGINT NOT NULL,
+
+  FOREIGN KEY (player_id) REFERENCES players (id)
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 
-CREATE EXTENSION btree_gist;
-CREATE TABLE player_sessions
-(
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE TABLE IF NOT EXISTS player_sessions (
     player_id INTEGER  NOT NULL,
     server_id SMALLINT NOT NULL,
     enter     BIGINT   NOT NULL,
@@ -116,7 +121,7 @@ CREATE TABLE player_sessions
     FOREIGN KEY (server_id) REFERENCES servers (id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE INDEX player_sessions_range ON player_sessions USING GiST (server_id, range);
-CREATE INDEX player_sessions_by_leave ON player_sessions (server_id, player_id, UPPER(range));
+CREATE INDEX IF NOT EXISTS player_sessions_range ON player_sessions USING GiST (server_id, range);
+CREATE INDEX IF NOT EXISTS player_sessions_by_leave ON player_sessions (server_id, player_id, UPPER(range));
 
-CREATE VIEW online_players AS SELECT * FROM player_sessions WHERE range @> (~(1::bigint << 63) >> 1);
+CREATE VIEW IF NOT EXISTS online_players AS SELECT * FROM player_sessions WHERE range @> (~(1::bigint << 63) >> 1);
