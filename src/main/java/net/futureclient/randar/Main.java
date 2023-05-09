2bbb1015352fe1c4532a8202cdbc6e0ec0e443e7
Babbaj <babbaj45@gmail.com>
2023-05-09 01:44:54 -0400

    remove hello world lol

--- src/main/java/net/futureclient/randar/Main.java
+++ src/main/java/net/futureclient/randar/Main.java
@@ -80,7 +80,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         new Database();
         eventProcessingThread = new Thread(() -> {
             while (true) {
