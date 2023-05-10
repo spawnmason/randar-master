c2def490ab9c2cd0f0c1a9264553c95d2bcd9e16
Babbaj <babbaj45@gmail.com>
2023-05-09 22:52:21 -0400

    didnt mean to commit that

--- src/main/java/net/futureclient/randar/Main.java
+++ src/main/java/net/futureclient/randar/Main.java
@@ -80,7 +80,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        if (true) return;
         new Database();
         eventProcessingThread = new Thread(() -> {
             while (true) {
