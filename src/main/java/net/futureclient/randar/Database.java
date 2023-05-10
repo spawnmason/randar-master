f646da740ad48f494ad40497bfbf66286c299669
Leijurv <leijurv@gmail.com>
2023-05-09 23:55:16 -0700

    100k limit not valid

--- src/main/java/net/futureclient/randar/Database.java
+++ src/main/java/net/futureclient/randar/Database.java
@@ -228,7 +228,7 @@ public class Database {
     }
 
     public static int copyNewSeedEvents(Connection con, long maxSeedID) throws SQLException {
-        try (PreparedStatement statement = con.prepareStatement("INSERT INTO rng_seeds_not_yet_processed (server_id, dimension, received_at, rng_seed) SELECT (SELECT id FROM servers WHERE name = event->>'server'), (event->'dimension')::smallint, (event->'timestamp')::bigint, jsonb_array_elements_text(event->'seeds')::bigint FROM events WHERE id > (SELECT id FROM seed_copy_progress) AND id <= ? AND event->>'type' = 'seed' ORDER BY id LIMIT 100000 ON CONFLICT DO NOTHING")) {
+        try (PreparedStatement statement = con.prepareStatement("INSERT INTO rng_seeds_not_yet_processed (server_id, dimension, received_at, rng_seed) SELECT (SELECT id FROM servers WHERE name = event->>'server'), (event->'dimension')::smallint, (event->'timestamp')::bigint, jsonb_array_elements_text(event->'seeds')::bigint FROM events WHERE id > (SELECT id FROM seed_copy_progress) AND id <= ? AND event->>'type' = 'seed' ORDER BY id ON CONFLICT DO NOTHING")) {
             statement.setLong(1, maxSeedID);
             return statement.executeUpdate();
         }
